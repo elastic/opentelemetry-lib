73bb483e2b2f9722c6f86c1c1cdc47569a4a96de
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-05-21 16:32:01 +0200

    Adding stacktrace without message to testdata

--- enrichments/logs/internal/mobile/groupingkey_test.go
+++ enrichments/logs/internal/mobile/groupingkey_test.go
@@ -1,1 +1,21 @@
 package mobile
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateGroupingKeyForStacktrace(t *testing.T) {
+	stacktrace_path := filepath.Join("testdata", "stacktrace1_a.txt")
+	bytes, err := os.ReadFile(stacktrace_path)
+	if err != nil {
+		assert.Fail(t, "Could not read test file")
+	}
+	stacktrace := string(bytes)
+
+	fmt.Printf("The stacktrace: %s", stacktrace)
+}
