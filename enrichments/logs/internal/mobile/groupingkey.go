8e2d593a3092807492590685ba45139ecbb02183
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-05-22 07:00:49 +0200

    Creating grouping key for java stacktraces

--- enrichments/logs/internal/mobile/groupingkey.go
+++ enrichments/logs/internal/mobile/groupingkey.go
@@ -1,8 +1,17 @@
 package mobile
 
-import "regexp"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"regexp"
+)
 
-func curateStacktrace(s string) string {
-	unwanted_pattern := regexp.MustCompile("(:\\s.+)|[\r\n\\s]+")
-	return unwanted_pattern.ReplaceAllString(s, "")
+func CreateGroupingKey(stacktrace string) string {
+	hash_bytes := sha256.Sum256([]byte(curateStacktrace(stacktrace)))
+	return hex.EncodeToString(hash_bytes[:])
+}
+
+func curateStacktrace(stacktrace string) string {
+	unwanted_pattern := regexp.MustCompile(`(:\s.+)|[\r\n\s]+`)
+	return unwanted_pattern.ReplaceAllString(stacktrace, "")
 }
--- enrichments/logs/internal/mobile/groupingkey_test.go
+++ enrichments/logs/internal/mobile/groupingkey_test.go
@@ -28,6 +28,26 @@ func TestCurateStacktrace(t *testing.T) {
 	}
 }
 
+func TestCreateGroupingKey(t *testing.T) {
+	for _, tc := range []struct {
+		stacktraces []string
+		expected_id string
+	}{
+		{
+			stacktraces: []string{readTestFile(t, "stacktrace1_a.txt"), readTestFile(t, "stacktrace1_b.txt")},
+			expected_id: "fbda93aa3a0ccb785da705ff03697bb7393cb0738e31c596e797b16ea2ba2f02",
+		},
+		{
+			stacktraces: []string{readTestFile(t, "stacktrace2_a.txt"), readTestFile(t, "stacktrace2_b.txt"), readTestFile(t, "stacktrace2_c.txt")},
+			expected_id: "10b6726bc6d9aab69389b8b5dfbd9df1a6e08db75b0777b4f2d0456adeaed50e",
+		},
+	} {
+		for _, stacktrace := range tc.stacktraces {
+			assert.Equal(t, tc.expected_id, CreateGroupingKey(stacktrace))
+		}
+	}
+}
+
 func readTestFile(t *testing.T, file_name string) string {
 	bytes, err := os.ReadFile(filepath.Join("testdata", file_name))
 	if err != nil {
